94ebb7688ad42f6c0efb954ec86f7a8dc207863a
shruthis00 <43936405+shruthis00@users.noreply.github.com>
2021-07-01 09:37:59 -0500

    Add files via upload

---
+++ T1_class_Srinivasan.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 30 14:20:13 2021
+
+@author: shrut
+"""
+import dipy
+from dipy.io.image import load_nifti, save_nifti
+import numpy as np
+from dipy.segment.tissue import TissueClassifierHMRF
+
+from os.path import join, expanduser
+
+home = expanduser('~')
+dname = join(home, '.dipy', 'sample_files', 'S00393')
+
+fdwi = join(dname, 'S00393_Reg_LPCA_nii3D.nii')
+fbval = join(dname, 'S00393_bvals.txt')
+fbvec = join(dname, 'S00393_bvecs.txt')
+
+data, affine, img= load_nifti(fdwi, return_img=True)
+tissue_data = np.asanyarray(img.dataobj)
+
+print(tissue_data.shape)
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+a = fig.add_subplot(1, 2, 1)
+img_ax = np.rot90(tissue_data[..., 89])
+imgplot = plt.imshow(img_ax, cmap="gray")
+a.axis('off')
+a.set_title('Axial')
+a = fig.add_subplot(1, 2, 2)
+img_cor = np.rot90(tissue_data[:, 128, :])
+imgplot = plt.imshow(img_cor, cmap="gray")
+a.axis('off')
+a.set_title('Coronal')
+plt.savefig('t1_image.png', bbox_inches='tight', pad_inches=0)
+
+#specifying types of classes
+nclass = 3
+
+#smoothness factor of segmentation
+beta = 0.1
+
+#Set number of iterations
+import time
+t0 = time.time()
+
+hmrf = TissueClassifierHMRF()
+initial_segmentation, final_segmentation, PVE = hmrf.classify(tissue_data, nclass, beta)
+
+t1 = time.time()
+total_time = t1-t0
+print('Total time:' + str(total_time))
+
+fig = plt.figure()
+a = fig.add_subplot(1, 2, 1)
+img_ax = np.rot90(final_segmentation[..., 89])
+imgplot = plt.imshow(img_ax)
+a.axis('off')
+a.set_title('Axial')
+a = fig.add_subplot(1, 2, 2)
+img_cor = np.rot90(final_segmentation[:, 128, :])
+imgplot = plt.imshow(img_cor)
+a.axis('off')
+a.set_title('Coronal')
+plt.savefig('final_seg.png', bbox_inches='tight', pad_inches=0)
+
+csf = PVE[:,:,:,0]
+gm = PVE[:,:,:,1]
+wm = PVE[:,:,:,2]
+
+fig = plt.figure()
+a = fig.add_subplot(1, 3, 3)
+img_cor = np.rot90(PVE[:, :, 89, 2])
+imgplot = plt.imshow(img_cor, cmap="gray")
+a.axis('off')
+a.set_title('White Matter')
+
+save_nifti('White_matter_mask_S00393', wm, affine)
---
+++ affine_Srinivasan.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 30 00:56:40 2021
+
+@author: shrut
+"""
+
+import dipy
+import numpy as np
+from os.path import join, expanduser
+from dipy.viz import regtools
+from dipy.io.image import load_nifti
+from dipy.align import (affine_registration, 
+                        center_of_mass, translation,
+                        rigid, affine, register_dwi_to_template)
+from dipy.align.imaffine import (transform_centers_of_mass,
+                                 AffineMap,
+                                 MutualInformationMetric,
+                                 AffineRegistration)
+from dipy.align.transforms import (TranslationTransform3D,
+                                   RigidTransform3D,
+                                   AffineTransform3D)
+home = expanduser('~')
+
+dname = join(home, '.dipy', 'sample_files')
+
+fdwi1 = join(dname, 'S00393', 'S00393_Reg_LPCA_nii3D.nii')
+fdwi2 = join(dname, 'S00490', 'S00490_3D_vol8.nii')
+
+s_data, s_affine, s_img = load_nifti(fdwi1, return_img = True)
+m_data, m_affine, m_img = load_nifti(fdwi2, return_img = True)
+
+identity = np.eye(4)
+affine_map = AffineMap(identity,
+                       s_data.shape, s_affine,
+                       m_data.shape, m_affine)
+resampled = affine_map.transform(m_data)
+regtools.overlay_slices(s_data, resampled, None, 0,
+                        "Static", "Moving", "resampled_0.png")
+regtools.overlay_slices(s_data, resampled, None, 1,
+                        "Static", "Moving", "resampled_1.png")
+regtools.overlay_slices(s_data, resampled, None, 2,
+                        "Static", "Moving", "resampled_2.png")
+
+nbins = 32
+sampling_prop = None
+metric = MutualInformationMetric(nbins, sampling_prop)
+
+#three levels of iterations, 10000 coarse, 1000 medium, and 100 fine. Feel free to change
+level_iters = [10000, 1000, 100]
+sigmas = [3.0, 1.0, 0.0]
+factors = [4, 2, 1]
+#All of this is default ^^ feel free to change
+
+"""
+affreg = AffineRegistration(metric=metric, level_iters=level_iters, sigmas=sigmas, factors=factors)
+
+transform = AffineTransform3D()
+params0 = None
+starting_affine = rigid.affine
+affine = affreg.optimize(s_data, m_data, transform, params0,
+                         static_affine, moving_affine,
+                         starting_affine=starting_affine)
+"""
+
+pipeline = [center_of_mass, translation, rigid, affine]
+
+transform, reg_affine = affine_registration(m_data, s_data, moving_affine = m_affine,
+                                            static_affine = s_affine, nbins = 32,
+                                            metric = 'MI', pipeline = pipeline,)
+
+regtools.overlay_slices(s_data, transform, None, 0,
+                        "Static", "Transformed", "xformed_affine_0.png")
+regtools.overlay_slices(s_data, transform, None, 1,
+                        "Static", "Transformed", "xformed_affine_1.png")
+regtools.overlay_slices(s_data, transform, None, 2, 
+                        "Static", "Transformed", "xformed_affine_2.png")
+
